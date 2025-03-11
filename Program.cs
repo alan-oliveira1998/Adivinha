c106c56a9bb85a0ae67cae51199156268c09d01b
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-11 20:04:24 -0300

    Resolvi o exercício

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("O que é, o que é?");
+Thread.Sleep(1000);
+Console.WriteLine();
+Console.Write("Feito para andar e não anda.");
+Thread.Sleep(5000);
+Console.WriteLine();
+Console.Write("A rua.");
